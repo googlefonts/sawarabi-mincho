64b2df33564c5328826fdb257408c336ab281050
mshio <mshio@54a90f34-5e62-402c-8eae-46c47f0b2e07>
2014-02-23 06:51:02 +0000

    make users jump to the mobile site when they come with mobile devices
    git-svn-id: svn+ssh://svn.osdn.net/svnroot/sawarabi-fonts/trunk@128 54a90f34-5e62-402c-8eae-46c47f0b2e07

--- html/js/mobile.js
+++ html/js/mobile.js
@@ -1,4 +1,7 @@
 (function() {
     var o = function(d) { return navigator.userAgent.indexOf(d) > 0; };
-    if (o('iPhone') || o('iPod') || o('Android')) location.href = './mobile/index.html';
+    if (o('iPhone') || o('iPod') || o('Android')) {
+	path = location.pathname;
+	location.href = '/mobile' + path;
+    }
 })();
